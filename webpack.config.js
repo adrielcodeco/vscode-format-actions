4af222ae26c132a96b26791168b9922e2c77e94a
codeco <adrielcodeco@hotmail.com>
2020-09-19 16:58:43 -0300

    fix: budle dependecies

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path')
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+const tsconfigBuild = require('./tsconfig.build.json')
+
+const root = __dirname
+const resolve = uri => path.resolve(root, uri)
+
+module.exports = {
+  entry: './src/extension.ts',
+  target: 'node',
+  devtool: 'source-map',
+  mode: 'production',
+  output: {
+    filename: 'extension.js',
+    path: resolve(tsconfigBuild.compilerOptions.outDir),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              configFile: 'tsconfig.build.json',
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    plugins: [new TsconfigPathsPlugin({ configFile: './tsconfig.build.json' })],
+  },
+  externals: [/^vscode/i],
+  resolveLoader: {
+    modules: [resolve('./node_modules')],
+  },
+  optimization: {
+    providedExports: true,
+    usedExports: true,
+    sideEffects: true,
+  },
+}
